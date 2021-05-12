2c1b795b6fb981644350e2887d73e07b4e841b04
email <jamestina18@gmail.com>
2021-05-12 05:16:51 -0700

    now updated with form and render function

--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect          
+from .models import *
 # Create your views here.
+def index(request):
+     all_users = User.objects.all()
+     context ={
+          "all_users": all_users
+
+     }
+     return render (request,'index.html', context)
+
+def add_user(request):
+     if request.method == "POST":
+          first_name = request.POST['first_name']
+          last_name = request.POST['last_name']
+          email_address = request.POST['email_address']
+          age = request.POST['age']
+          User.objects.create(first_name=first_name,last_name=last_name, email_address=email_address, age=age)
+     return redirect("/")
